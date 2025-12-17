d61848f68d0d8bc5d432bbb55f32b0836b80453a
putri479 <putriismailll285@gmail.com>
2025-12-17 13:15:10 +0700

    Melakukan Perubahan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>AdminLTE v4 | Dashboard</title>
+    <!-- Aku Adalah Anak Gembala Selalu Riang Serta Gembira -->
 
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
